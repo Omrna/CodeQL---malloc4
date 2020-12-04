202f07c22c1b50fdeaf30f646d699351eefe3b5e
Omran Alhaddad <44875477+Omrna@users.noreply.github.com>
2020-12-04 21:19:14 +0000

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-#include <string.h>
+#include<string.h>
 
 #define SECRET1 0x44
 #define SECRET2 0x55
@@ -16,9 +16,9 @@ int main(int argc, char *argv[])
 
   
     char *str = malloc(strlen("hello"));
-    char *str2 = malloc(strlen("hello")+0);
-    char *str2 = malloc(strlen("hello")-1);
-    char *str3 = malloc(strlen("hello")+1);
   
+    char *str2 = malloc(strlen("hello")+0);
+    char *str3 = malloc(strlen("hello")-1);
+    char *str4 = malloc(strlen("hello")+1);
     return 0;
 }
